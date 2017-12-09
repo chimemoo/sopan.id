87e0b99457bfbc0748d7abd8b214ea788f1d47e4
Christ Memory Sitorus <34398865+chimemoo@users.noreply.github.com>
2017-12-09 22:18:01 +0700

    Add files via upload

---
+++ Sopan.id/home.php
@@ -0,0 +1,352 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="assets/img/favicon.png">
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <title>[Beta 0.1] Sopan.id | StartUp Bidang Internet - Mari Ber-internet yang Sehat</title>
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+    <link href="assets/css/gaia.css" rel="stylesheet"/>
+
+    <!--     Fonts and icons     -->
+    <link href='https://fonts.googleapis.com/css?family=Cambo|Poppins:400,600' rel='stylesheet' type='text/css'>
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+    <link href="assets/css/fonts/pe-icon-7-stroke.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+</head>
+
+<body>
+
+    <nav class="navbar navbar-default navbar-transparent navbar-fixed-top" color-on-scroll="200">
+        <!-- if you want to keep the navbar hidden you can add this class to the navbar "navbar-burger"-->
+        <div class="container">
+            <div class="navbar-header">
+                <button id="menu-toggle" type="button" class="navbar-toggle" data-toggle="collapse" data-target="#example">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar bar1"></span>
+                    <span class="icon-bar bar2"></span>
+                    <span class="icon-bar bar3"></span>
+                </button>
+                <a href="#" class="navbar-brand">
+                    Sopan.id  <sup><span class="label label-primary">Beta</span><sup>
+                </a>
+            </div>
+            <div class="collapse navbar-collapse">
+                <ul class="nav navbar-nav navbar-right navbar-uppercase">
+                    <li>
+                        <a href="#Tentang">Tentang Kami</a>
+                    </li>
+                    <li>
+                        <a href="#">Tujuan Kami</a>
+                    </li>
+                    <li>
+                        <a href="#">Dukung Kami</a>
+                    </li>
+                    <li class="dropdown">
+                        <a href="#gaia" class="dropdown-toggle" data-toggle="dropdown">
+                            <i class="fa fa-share-alt"></i> Share
+                        </a>
+                        <ul class="dropdown-menu dropdown-primary">
+                            <li>
+                                <a href="#"><i class="fa fa-facebook-square"></i> Facebook</a>
+                            </li>
+                            <li>
+                                <a href="#"><i class="fa fa-twitter"></i> Twitter</a>
+                            </li>
+                            <li>
+                                <a href="#"><i class="fa fa-instagram"></i> Instagram</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+    </nav>
+
+
+    <div class="section section-header">
+        <div class="parallax filter filter-color-blue">
+           
+            <div class="container">
+                <div class="content">
+                    <div class="title-area">
+                        <h1 class="title-modern">Sopan.id</h1>
+                        <h3>Startup Bidang Internet - Mari Ber-internet yang Sehat</h2>
+                        <div class="separator line-separator">♦</div>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+
+    <div class="section">
+        <div class="container">
+            <div class="row">
+                <div class="title-area">
+                    <h2 id="Tentang">Tentang Kami</h2>
+                    <div class="separator separator-danger">✻</div>
+                    <p class="description">Sopan.id adalah Startup yang fokus pada edukasi internet dan bersosial media yang sehat dan juga fokus pada pengembangan aplikasi pemfilteran konten yang tidak sesuai dengan etika dan norma kesopanan di Indonesia.</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="info-icon">
+                        <div class="icon text-primary">
+                            <i class="pe-7s-graph1"></i>
+                        </div>
+                        <h3>Infografis</h3>
+                        <p class="description">Kami membuat informasi tentang internet dan sosial media menggunakan media infografis supaya mudah dipahami oleh orang awam.</p>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="info-icon">
+                        <div class="icon text-primary">
+                            <i class="pe-7s-note2"></i>
+                        </div>
+                        <h3>Artikel</h3>
+                        <p class="description">Kami menyediakan informasi bacaan yang berkaitan dengan edukasi penggunaan internet dan sosial media dalam bentuk artikel pada blog kami.</p>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="info-icon">
+                        <div class="icon text-primary">
+                            <i class="pe-7s-play"></i>
+                        </div>
+                        <h3>Aplikasi</h3>
+                        <p class="description">Kedepannya kami akan mengembangkan aplikasi filtering pada konten-konten internet yang sesuai dengan visi dari Startup kami.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="section section-our-team-freebie">
+        <div class="parallax filter filter-color-black">
+            <div>
+            </div>
+            <div class="container">
+                <div class="content">
+                    <div class="row">
+                        <div class="title-area">
+                            <h2>Latar Belakang</h2>
+                            <div class="separator separator-primary">✻</div>
+                        </div>
+                    </div>
+
+                    <div class="team">
+                        <div class="row">
+                            <div class="col-md-10 col-md-offset-1">
+                                <div class="row">
+                                    <div class="col-md-4">
+                                        <div class="card card-member">
+                                            <div class="content">
+                                                <div class="description">
+                                                    <h3 class="title">Literasi dan Kesopanan</h3>
+                                                    <p class="description">Pembuatan Sopan.id di dilatar belakangi oleh rasa kepedulian pada Netizen indonesia yang masih buruk dalam literasi dan norma kesopanan. Serta kepedulian pada masyarakat yang baru mulai mengenal internet, agar mereka dapat menggunakan internet dengan bijak</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <div class="card card-member">
+                                            <div class="content">
+                                                <div class="description">
+                                                    <h3 class="title">HOAX</h3>
+                                                    <p class="description">Munculnya berita-berita Hoax saat ini sangat meresahkan dan bahkan menimbulkan perpecahan di masyarakat, apalagi ketika berita tersebut ditanggapi masyarakat dengan cara yang kurang tepat seperti dengan memberikan kata-kata kotor.</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <div class="card card-member">
+                                            <div class="content">
+                                                <div class="description">
+                                                    <h3 class="title">Bullying</h3>
+                                                    <p class="description">Ahir-akhir ini kasus Bullying bukan hanya terjadi di dunia nyata namun juga di dunia maya. Komentar-komentar pedas dan memojokkan dapat menimbulkan efek depresi berat bagi korbannya.</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="section section-our-clients-freebie">
+        <div class="container">
+            <div class="title-area">
+                <h5 class="subtitle text-gray">Kami memiliki tujuan</h5>
+                <h2>Tujuan kami</h2>
+                <div class="separator separator-primary">∎</div>
+            </div>
+
+
+            <div class="tab-content">
+                <div class="tab-pane active">
+                    <p class="description">
+                        1. Memberikan konten-konten Artikel dan Infografis yang bertujuan mengedukasi pengguna internet tentang norma-norma kesopanan dan cara ber-literasi yang baik.<br>
+                        2. Melakukan blokir pada konten-konten website dan social media yang tidak sesuai dengan kriteria kami.<br>
+                        3. Membuat Aplikasi filtering yang dapat men-filter konten yang tidak bermanfaat agar tidak ditampilkan. 
+                    </p>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+
+    <div class="section section-small section-get-started">
+        <div class="parallax filter">
+            <div class="image"
+                style="background-image: url('assets/img/office-1.jpeg')">
+            </div>
+            <div class="container">
+                <div class="title-area">
+                    <h2 class="text-white">Ayo Bergabung Dengan Kami!</h2>
+                    <div class="separator line-separator">♦</div>
+                    <p class="description"> Kami mencari relawan yang memiliki keinginan yang sama dengan kami. Tidak perlu memiliki kemampuan coding atau berliterasi yang bagus, kami hanya meminta anda untuk memberikan konten yang sesuai etika kesopanan dan juga melakukan pelaporan pada konten buruk yang anda temui.</p>
+                </div>
+
+                <div class="button-get-started">
+                    <a href="#gaia" class="btn btn-primary btn-fill btn-lg filter-color-blue">Bergabung dengan kami</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <footer class="footer footer-big footer-color-black" data-color="black">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-2 col-sm-3">
+                    <div class="info">
+                        <h5 class="title">Sopan.id</h5>
+                        <nav>
+                            <ul>
+                                <li>
+                                    <a href="#">Home</a></li>
+                                <li>
+                                    <a href="#">Menjadi kontributor</a>
+                                </li>
+                                <li>
+                                    <a href="#">Bergabung dengan Projek kami</a>
+                                </li>
+                                <li>
+                                    <a href="#">Portofolio kami</a>
+                                </li>
+                                <li>
+                                    <a href="#">Tentang kami</a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+                <div class="col-md-3 col-md-offset-1 col-sm-3">
+                    <div class="info">
+                        <h5 class="title"> Bantuan</h5>
+                         <nav>
+                            <ul>
+                                <li>
+                                    <a href="#">Hubungi kami</a>
+                                </li>
+                                <li>
+                                    <a href="#">Bagaimana menjadi relawan</a>
+                                </li>
+                                <li>
+                                    <a href="#">Syarat &amp; Ketentuan</a>
+                                </li>
+                                <li>
+                                    <a href="#">Kebijakan Kami</a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-3">
+                    <div class="info">
+                        <h5 class="title">Latest News</h5>
+                        <nav>
+                            <ul>
+                                <li>
+                                    <a href="#">
+                                        <i class="fa fa-twitter"></i> <b>Get Shit Done</b> The best kit in the market is here, just give it a try and let us...
+                                        <hr class="hr-small">
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <i class="fa fa-twitter"></i> We've just been featured on <b> Awwwards Website</b>! Thank you everybody for...
+                                    </a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+                <div class="col-md-2 col-md-offset-1 col-sm-3">
+                    <div class="info">
+                        <h5 class="title">Follow us on</h5>
+                        <nav>
+                            <ul>
+                                <li>
+                                    <a href="#" class="btn btn-social btn-facebook btn-simple">
+                                        <i class="fa fa-facebook-square"></i> Facebook
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="btn btn-social btn-dribbble btn-simple">
+                                        <i class="fa fa-dribbble"></i> Dribbble
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="btn btn-social btn-twitter btn-simple">
+                                        <i class="fa fa-twitter"></i> Twitter
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="btn btn-social btn-reddit btn-simple">
+                                        <i class="fa fa-google-plus-square"></i> Google+
+                                    </a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+            <hr>
+            <div class="copyright">
+                 © <script> document.write(new Date().getFullYear()) </script> Creative Tim, made with love
+            </div>
+        </div>
+    </footer>
+
+</body>
+
+<!--   core js files    -->
+<script src="assets/js/jquery.min.js" type="text/javascript"></script>
+<script src="assets/js/bootstrap.js" type="text/javascript"></script>
+
+<!--  js library for devices recognition -->
+<script type="text/javascript" src="assets/js/modernizr.js"></script>
+
+<!--  script for google maps   -->
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js"></script>
+
+<!--   file where we handle all the script from the Gaia - Bootstrap Template   -->
+<script type="text/javascript" src="assets/js/gaia.js"></script>
+
+</html>
